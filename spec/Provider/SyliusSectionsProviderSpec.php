ba1a5d05eb1ee370d4aa5f3939c01d5911ac86fa
Ahmed Aziz Abbassi <ahmedaziz.abbassi.dev@gmail.com>
2025-08-04 15:57:46 +0100

    fix: Update SyliusSectionsProviderSpec and Configuration for cities_management
    - Added 'app_city' to the 'configuration' section in SyliusSectionsProviderSpec.
    - Reinstate 'cities_management' in the custom_sections of Configuration.php, ensuring proper handling of cities management in the application.
    - Adjusted config.yaml to include 'cities_management' under custom_sections.

--- spec/Provider/SyliusSectionsProviderSpec.php
+++ spec/Provider/SyliusSectionsProviderSpec.php
@@ -34,7 +34,6 @@ final class SyliusSectionsProviderSpec extends ObjectBehavior
                     'sylius_admin_tax_category',
                     'sylius_admin_tax_rate',
                     'sylius_admin_zone',
-                    'app_city',
                 ],
                 'customers_management' => [
                     'sylius_admin_customer',
@@ -59,6 +58,9 @@ final class SyliusSectionsProviderSpec extends ObjectBehavior
                 'custom' => [
                     'rbac' => [
                         'odiseo_sylius_rbac_plugin',
+                    ],
+                    'configuration' => [
+                        'app_city',
                     ],
                 ],
             ]
